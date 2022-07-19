0ac1a13f0b1c22ad26cf91f8f3d530bed88a4c6b
sulemanjoseph <sulemanjoseph7@gmail.com>
2022-07-19 11:48:18 +0500

    make it better

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./app.css";
+import Navber from "./navber";
+import Home from "./home";
+import Brand from "./brand";
+import Contact from "./contact";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navber />
+      <Routes>
+        <Route path="//Home" element={<Home />} />
+        <Route path="/brand" element={<Brand />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
